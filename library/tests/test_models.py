a0a4735ea4cf6dba3a4b46646b99684728eabbea
namuhuchutong <namuhuchutong@gmail.com>
2021-07-22 19:53:18 +0900

    test: add author, language, book test cases

--- library/tests/test_models.py
+++ library/tests/test_models.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from library.models import Author
+from library.models import Genre
+from library.models import Language
+from library.models import Book
+from library.models import BookInstance
+from datetime import date
 
 class AuthorModelTest(TestCase):
 
@@ -34,3 +39,83 @@ class AuthorModelTest(TestCase):
     def test_absolute_url(self):
         # before test this function. first set urls config
         pass
+
+
+class GenreModelTest(TestCase):
+
+    def setUp(self):
+        Genre.objects.create(name="Test genre")
+    
+    def test_name_label(self):
+        genre = Genre.objects.get(id=1)
+        name_label = genre._meta.get_field('name')
+        self.assertEquals(name_label.name, 'name')
+
+    def test_object_name(self):
+        genre = Genre.objects.get(id=1)
+        object_name = genre.name
+        self.assertEquals(str(genre), object_name)
+
+    def test_name_length(self):
+        genre = Genre.objects.get(id=1)
+        max_length = genre._meta.get_field('name').max_length
+        self.assertEquals(max_length, 200)
+
+
+class LanguageModelTest(TestCase):
+
+    def setUp(self):
+        Language.objects.create(name="Test genre")
+    
+    def test_name_label(self):
+        language = Language.objects.get(id=1)
+        name_label = language._meta.get_field('name')
+        self.assertEquals(name_label.name, 'name')
+
+    def test_object_name(self):
+        language = Language.objects.get(id=1)
+        object_name = language.name
+        self.assertEquals(str(language), object_name)    
+    
+    def test_name_length(self):
+        language = Language.objects.get(id=1)
+        max_length = language._meta.get_field('name').max_length
+        self.assertEquals(max_length, 200)
+
+
+class BookModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Author_ = Author.objects.create(
+            name="Tester",
+            date_of_birth=date.today(),
+            date_of_death=date.today()
+        )
+        Genre_ = Genre.objects.create(
+            name="Test genre"
+        )
+        Language_ = Language.objects.create(
+            name="ENG"
+        )
+        instance = Book.objects.create(
+            title="Test title",
+            author=Author_,
+            isbn="12312412412",
+            language=Language_
+        )
+        instance.genre.set(Genre.objects.filter(name="Test genre"))
+
+    def test_title_label(self):
+        book = Book.objects.get(id=1)
+        title_label = book._meta.get_field('title')
+        self.assertEquals(title_label.name, 'title')
+
+    def test_created_properly(self):
+        self.assertEquals(
+            1, 
+            len(Book.objects.filter(
+                author__in=Author.objects.filter(name="Tester")
+                )
+            )
+        )
