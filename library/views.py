5fa2979486553bad7c286302fdd4ef7e9e35e47b
namuhuchutong <namuhuchutong@gmail.com>
2021-07-24 18:25:32 +0900

    feat: add author, book list

--- library/views.py
+++ library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    pass
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = "library/book_list.html"
+    paginate_by = 10
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = "library/author_list.html"
+    paginate_by = 10
